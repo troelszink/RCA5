e1b2abf442fcaed3456fb538cf33aaf67f456343
Jakob Wilm <jakw@dtu.dk>
2019-09-13 13:47:12 +0200

    Update gazebo_client.sh

--- gazebo_client.sh
+++ gazebo_client.sh
@@ -6,7 +6,7 @@ DIR="$( cd "$( dirname "$1" )" >/dev/null && pwd )"
 echo $DIR
 export GAZEBO_MODEL_PATH=$DIR/models/:$GAZEBO_MODEL_PATH 
 
-export GAZEBO_PLUGIN_PATH=/usr/lib/x86_64-linux-gnu/gazebo-9/plugins/:$GAZEBO_PLUGIN_PATH
+export GAZEBO_PLUGIN_PATH=/usr/lib/x86_64-linux-gnu/gazebo-10/plugins/:$GAZEBO_PLUGIN_PATH
 export GAZEBO_PLUGIN_PATH=$DIR/marble_contact_plugin/build:$GAZEBO_PLUGIN_PATH
 
 gzclient
