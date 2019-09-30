bac8c89dfc57d99d186bffa3e2acdd48a4422855
Andreas Kjeldsgaard Jensen <ankj17@student.sdu.dk>
2019-09-30 03:46:29 -0700

    First commit

---
+++ robot_control/build/CMakeFiles/robot_control.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/robot_control.dir/src/main.cpp.o"
+  "robot_control.pdb"
+  "robot_control"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/robot_control.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
