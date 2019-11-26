e395b356019cf14cecd9a24a318113cf90785fcc
ankj17 <44611254+ankj17@users.noreply.github.com>
2019-11-26 17:29:09 +0100

    Add files via upload

--- robot_control/src/QLearning.cpp
+++ robot_control/src/QLearning.cpp
@@ -32,10 +32,21 @@ void QLearning::initMap()
 
 void QLearning::printEnvironment()
 {
+	for (int y = 0; y < ROWS; y++)				//testWorld
+	{
+		for (int x = 0; x < COLUMNS; x++)
+			std::cout << "" << environment[y][x];
+
+		std::cout << std::endl;
+	}
+}
+
+void QLearning::printQTable()
+{
 	for (int y = 0; y < ROWS; y++)
 	{
 		for (int x = 0; x < COLUMNS; x++)
-				std::cout << "" << environment[y][x];
+			std::cout << " " << Q[y][x].UP;
 
 		std::cout << std::endl;
 	}
@@ -43,24 +54,42 @@ void QLearning::printEnvironment()
 
 void QLearning::insertMarbles()
 {
-	environment[5][5] = '+';
-	environment[5][60] = '+';
-	environment[5][40] = '+';
-	environment[40][10] = '+';
-	environment[70][10] = '+';
-	environment[30][110] = '+';
-	environment[70][110] = '+';
-	environment[65][20] = '+';
-	environment[70][50] = '+';
-	environment[50][55] = '+';
-	environment[70][70] = '+';
+	//environment[5][5] = '+';
+	//environment[5][60] = '+';
+	//environment[5][40] = '+';
+	//environment[40][10] = '+';
+	//environment[70][10] = '+';
+	//environment[25][110] = '+';
+	//environment[70][110] = '+';
+	//environment[65][20] = '+';
+	//environment[70][50] = '+';
+	//environment[50][55] = '+';
+	//environment[70][70] = '+';
+	//environment[40][55] = '+';
+
+	//environment[10][20] = 'r';
+	//environment[10][55] = 'r';
+	//environment[25][40] = 'r';
+	//environment[50][10] = 'r';
+	//environment[30][110] = 'r';
+	//environment[60][110] = 'r';
+	//environment[50][25] = 'r';
+	//environment[75][58] = 'r';
+	//environment[55][90] = 'r';
+	//environment[68][85] = 'r';
+
+
+	environment[3][1] = '+';				// Test World
+	environment[3][5] = '+';
+	environment[3][7] = '+';
+
 
 }
 
 // Get the next state given a current state s and an action a:
 state QLearning::GetNextState(state s, action a)
 {
-	if (environment[s.y][s.x] != ' ')
+	if (environment[s.y][s.x] == '#')										//Change for Bigworld
 		return TERMINAL_STATE;
 
 	switch (a) {
@@ -70,7 +99,7 @@ state QLearning::GetNextState(state s, action a)
 	case RIGHT:  s.x += 1; break;
 	}
 
-	if (s.x < 0 || s.y < 0 || s.x >= COLUMNS || s.y >= ROWS)
+	if (s.x < 0 || s.y < 0 || s.x >= COLUMNS || s.y >= ROWS || environment[s.y][s.x] == '#')
 		return TERMINAL_STATE;
 
 	s.is_outside_environment = false;
@@ -83,14 +112,17 @@ float QLearning::GetReward(state s, action a)
 	state next = GetNextState(s, a);
 	if (next.is_outside_environment)
 	{
-		return 0;
+		return -1;
 	}
 	else {
 		if (environment[next.y][next.x] == '+')
-			return 1.0;
+			return 10.0;
 
-		if (environment[next.y][next.x] == '-')
+		if (environment[next.y][next.x] == '#')
 			return -1.0;
+
+		if (environment[next.y][next.x] == 'r')
+			return 5;
 
 		return 0;
 	}
@@ -102,27 +134,75 @@ action QLearning::GetNextAction(state s)
 	action a;
 	action maxAction = RIGHT;
 
-	float maxReward = -2;
+	float random = (float)rand() / (float)RAND_MAX;
 
-	for (int i = 0; i < 4; i++)
+	if (random < epsilon)
 	{
+		int i = rand() % 4;
+		//std::cout << "Random action: " << i << std::endl;
+
 		switch (i) {
 		case 0:   a = UP; break;
 		case 1:   a = DOWN; break;
 		case 2:   a = LEFT; break;
 		case 3:   a = RIGHT; break;
 		}
-		if (!GetNextState(s, a).is_outside_environment) {
-			float vnext = GetReward(s, a) + discount_rate * Q[GetNextState(s, a).y][GetNextState(s, a).x];
+	}
+	else
+	{
+		a = GetNextBestAction(s);
+	}
 
-			if (vnext > maxReward)
-			{
-				maxAction = a;
-				maxReward = vnext;
-			}
+	return a;
+}
+
+action QLearning::GetNextBestAction(state s)
+{
+	action a;
+	float vnext[4];
+
+	if (!GetNextState(s, UP).is_outside_environment)
+		vnext[0] = Q[s.y][s.x].UP + alpha * (GetReward(s, UP) + discount_rate * (Q[GetNextState(s, UP).y][GetNextState(s, UP).x].UP) - Q[s.y][s.x].UP);
+	if (!GetNextState(s, DOWN).is_outside_environment) 
+		vnext[1] = Q[s.y][s.x].DOWN + alpha * (GetReward(s, DOWN) + discount_rate * (Q[GetNextState(s, DOWN).y][GetNextState(s, DOWN).x].DOWN) - Q[s.y][s.x].DOWN);
+	if (!GetNextState(s, LEFT).is_outside_environment)
+		vnext[2] = Q[s.y][s.x].LEFT + alpha * (GetReward(s, LEFT) + discount_rate * (Q[GetNextState(s, LEFT).y][GetNextState(s, LEFT).x].LEFT) - Q[s.y][s.x].LEFT);
+	if (!GetNextState(s, RIGHT).is_outside_environment)
+		vnext[3] = Q[s.y][s.x].RIGHT + alpha * (GetReward(s, RIGHT) + discount_rate * (Q[GetNextState(s, RIGHT).y][GetNextState(s, RIGHT).x].RIGHT) - Q[s.y][s.x].RIGHT);
+		
+	int maxReward = vnext[0];
+	int maxAction = 0;
+
+	for (int i = 0; i < 4; i++)
+	{
+		if (vnext[i] > maxReward)
+		{
+			maxAction = i;
+			maxReward = vnext[i];
 		}
 	}
-	return maxAction;
+
+	switch (maxAction) {
+	case 0:   a = UP; break;
+	case 1:   a = DOWN; break;
+	case 2:   a = LEFT; break;
+	case 3:   a = RIGHT; break;
+	}
+
+	return a;
+}
+
+
+int QLearning::getStateNumber(state s)
+{
+	int state_number;
+	if (s.y > 0)
+		state_number = (s.y) * COLUMNS + s.x;
+	else
+		state_number = s.x;
+
+
+	return state_number;
 }
 
 QLearning::~QLearning()
--- robot_control/src/QLearning.h
+++ robot_control/src/QLearning.h
@@ -1,19 +1,28 @@
 #pragma once
 #include <opencv2/opencv.hpp>
+#include <opencv2/core.hpp>
 
-#define COLUMNS 120
-#define ROWS 80
+//#define COLUMNS 120				// Bigworld
+//#define ROWS 80
+
+#define COLUMNS 10					// Test Environment
+#define ROWS 6
+
+//Actions:
+enum action { UP, DOWN, LEFT, RIGHT };
+
 
 struct state
 {
 	int x;
 	int y;
 	bool is_outside_environment;
+	float UP;
+	float DOWN;
+	float LEFT;
+	float RIGHT;
 };
 
-
-// Actions:
-enum action { UP, DOWN, LEFT, RIGHT };
 
 class QLearning
 {
@@ -22,27 +31,46 @@ public:
 
 	void initMap();
 	void printEnvironment();
+	void printQTable();
 	void insertMarbles();
 	state GetNextState(state s, action a);
 	float GetReward(state s, action a);
 	action GetNextAction(state s);
+	action GetNextBestAction(state s);
 
 
 	// Discount rate:
 	float discount_rate = 0.9;
 
 	// Learning rate:
-	float alpha = 0.7;
+	float alpha = 0.5;
 
 	// A convenient definition of the terminal state
 	const state TERMINAL_STATE = { -1, -1, true };
 
 	// Current estimate of state values under the current policy:
-	float Q[ROWS][COLUMNS];
+	struct state Q[ROWS][COLUMNS];
 
+	// Epsilon
+	float epsilon = 0.1;
+
+	// Maximum steps
+	int max_steps = 15;
+
+	char environment[ROWS][COLUMNS] = { { '#', '#', '#', '#', '#', '#', '#', '#', '#', '#' },			// alpha = 0.5  epsilon = 0.1 discount = 0.9
+										{ '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#' },
+										{ '#', ' ', '#', ' ', ' ', ' ', '#', ' ', ' ', '#' },
+										{ '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#' },
+										{ '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' ,'#' }, 
+										{ '#', '#', '#', '#', '#', '#', '#', '#', '#' , '#'} };
+
+	//char environment[ROWS][COLUMNS] = {};
+
+	int getStateNumber(state s);
 
 	~QLearning();
 private:
-	char environment[ROWS][COLUMNS] = {};
+	
+
 };
 
